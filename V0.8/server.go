7c784fe38fc283ed20925a52cebc4f6c5781fc7e
颜志成 <yanzhicheng@bingosoft.net>
2023-11-01 15:03:31 +0800

    last commit

--- V0.8/server.go
+++ V0.8/server.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
-	"io"
 	"time"
 )
 
@@ -32,13 +32,13 @@ func NewServer(ip string, port int) *Server {
 	return server
 }
 
-//广播消息
+// 广播消息
 func (s *Server) BroadCast(user *User, msg string) {
 	sendMes := "[" + user.Addr + "]" + user.Name + ":" + msg
 	s.Message <- sendMes
 }
 
-//监听Message channel 的goroutine，一旦有消息就发送给全部的User
+// 监听Message channel 的goroutine，一旦有消息就发送给全部的User
 func (s *Server) ListenMessage() {
 	for {
 		msg := <-s.Message
@@ -54,24 +54,24 @@ func (s *Server) ListenMessage() {
 func (s *Server) Handler(conn net.Conn) {
 	//业务
 	// fmt.Println("创建连接成功！")
-	user := NewUser(conn,s)
+	user := NewUser(conn, s)
 	//用户上线,将用户添加进OnlineMap
 	user.Online()
 
 	isAction := make(chan bool)
 
-	go func(){
-		buf := make([]byte,4096)
+	go func() {
+		buf := make([]byte, 4096)
 		//这边一定要一个死循环监听用户，不然用户只能发送一次消息
-		for{
-			n,err := conn.Read(buf)
+		for {
+			n, err := conn.Read(buf)
 			if n == 0 {
 				user.Offline()
 				return
 			}
 			if err != nil && err != io.EOF {
 				fmt.Sprintf("read is err:", err)
-				return 
+				return
 			}
 			//去除最后一个回车符号
 			msg := string(buf[:n-1])
@@ -79,14 +79,14 @@ func (s *Server) Handler(conn net.Conn) {
 			user.DoMessage(msg)
 
 			//代表当前用户是活跃的
-			isAction <-true
+			isAction <- true
 		}
 	}()
 
 	//当前handle阻塞
-	for{
+	for {
 		select {
-			//判断是否处于活跃，处于活跃则要重置监听时间
+		//判断是否处于活跃，处于活跃则要重置监听时间
 		case <-isAction:
 			//监听时间
 		case <-time.After(time.Second * 1000):
@@ -99,10 +99,10 @@ func (s *Server) Handler(conn net.Conn) {
 			//关闭连接
 			conn.Close()
 			//关闭当前处理 或者 runtime.Goexit()
-			return 
+			return
 		}
 	}
-	
+
 }
 
 func (s *Server) Start() {
@@ -112,9 +112,6 @@ func (s *Server) Start() {
 		fmt.Println("listen to err:", err)
 		return
 	}
-
-	
-
 	//close
 	defer listen.Close()
 
